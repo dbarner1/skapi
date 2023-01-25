7fa8c88ce1ca7fcbafbd5b26e301f1bfde9c6eae
dbarner <dbarner.social@gmail.com>
2023-01-24 19:59:40 -0500

    Convert app to use koa

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
-const express = require('express')
-const app = express();
+const http = require('http')
+const https = require('https')
+const Koa = require('koa');
+const app = new Koa();
 
-app.get("/", (req, res) => {
-  res.send(200, "<h1>Just a simple homepage</h1>")
-})
+/*
+  MIDDLEWARE
+*/
 
-app.listen(5000);
+// logger
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// response
+app.use(async ctx => {
+  const { request } = ctx;
+  if(request.url === '/') {
+    ctx.body = 'Welcome to the Scratch Kitchen'
+  } else {
+    ctx.body = 'Page not found.';
+  }
+});
+
+app.on('error', err => {
+  log.error('server error', err)
+});
+
+http.createServer(app.callback()).listen(5000);
+https.createServer(app.callback()).listen(5001);
